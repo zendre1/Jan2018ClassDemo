ab24de9bbb113da433f3dca7c9f0b737ee0a51fc
Don Welch <dwelch@nait.ca>
2018-01-19 18:01:20 -0700

    add CRUD methods to Albums

--- ChinookSystem/BLL/AlbumContrroller.cs
+++ ChinookSystem/BLL/AlbumContrroller.cs
@@ -35,6 +35,52 @@ namespace ChinookSystem.BLL
                 return context.Albums.Find(albumid);
             }
         }
+
+        [DataObjectMethod(DataObjectMethodType.Insert,false)]
+        public void Albums_Add(Album item)
+        {
+            using (var context = new ChinookContext())
+            {
+                //staged to be physical placed on the database
+                context.Albums.Add(item);
+                //physically cause the staged item to be
+                //    placed on the database
+                //this is the commit of the using transaction
+                context.SaveChanges();
+            }
+        }
+
+        [DataObjectMethod(DataObjectMethodType.Update, false)]
+        public void Albums_Update(Album item)
+        {
+            using (var context = new ChinookContext())
+            {
+                item.ReleaseLabel = string.IsNullOrEmpty(item.ReleaseLabel) ? null : item.ReleaseLabel;
+
+                context.Entry(item).State = System.Data.Entity.EntityState.Modified;
+                context.SaveChanges();
+            }
+        }
+
+        [DataObjectMethod(DataObjectMethodType.Delete, false)]
+        public void Albums_Delete(Album item)
+        {
+            Albums_Delete(item.AlbumId);
+        }
+
+        public void Albums_Delete(int albumid)
+        {
+            using (var context = new ChinookContext())
+            {
+                var existing = context.Albums.Find(albumid);
+                if (existing == null)
+                {
+                    throw new Exception("Album does not exists on file.");
+                }
+                context.Albums.Remove(existing);
+                context.SaveChanges();
+            }
+        }
     }
 }
 
