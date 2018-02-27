08340eb72ad4ef59f2c134c0730bfe16ea5246ec
Don Welch <dwelch@nait.ca>
2018-02-26 18:54:26 -0700

    setup ManagePlaylist page from a web site page

---
+++ Jan2018DemoWebsite/SamplePages/ManagePlaylist.aspx.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Jan2018DemoWebsite.SamplePages
+{
+    public partial class ManagePlaylist : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void ArtistFetch_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void MediaTypeFetch_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void GenreFetch_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void AlbumFetch_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void PlayListFetch_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void MoveDown_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void MoveUp_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void DeleteTrack_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void ListView1_ItemCommand(object sender, ListViewCommandEventArgs e)
+        {
+
+        }
+    }
+}
