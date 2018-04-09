47cc00d349d0e3604a5c459c677c7c21c79c8450
Don Welch <dwelch@nait.ca>
2018-04-09 17:34:37 -0600

    attempt to create a report

---
+++ Chinook.Data/POCOs/GenreAlbumReport.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chinook.Data.POCOs
+{
+    public class GenreAlbumReport
+    {
+        public string GenreName { get; set; }
+        public string AlbumTitle { get; set; }
+        public string TrackName { get; set; }
+        public int Milliseconds { get; set; }
+        public int? Bytes { get; set; }
+        public decimal UnitPrice { get; set; }
+    }
+}
--- ChinookSystem/BLL/TrackController.cs
+++ ChinookSystem/BLL/TrackController.cs
@@ -73,5 +73,29 @@ namespace ChinookSystem.BLL
                 return results.ToList();
             }
         }//eom
+
+        //this method will create a flat POCO data set
+        //   to use in reporting
+        [DataObjectMethod(DataObjectMethodType.Select,false)]
+        public List<GenreAlbumReport> GenreAlbumReport_Get()
+        {
+            using (var context = new ChinookContext())
+            {
+                //there is no need to sort this dataset because
+                //    the final report sort will be done
+                //    in the report
+                var results = from x in context.Tracks
+                              select new GenreAlbumReport
+                              {
+                                  GenreName = x.Genre.Name,
+                                  AlbumTitle = x.Album.Title,
+                                  TrackName = x.Name,
+                                  Milliseconds = x.Milliseconds,
+                                  Bytes = x.Bytes,
+                                  UnitPrice = x.UnitPrice
+                              };
+                return results.ToList();
+            }
+        }
     }
 }
